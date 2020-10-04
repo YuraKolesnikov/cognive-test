8097cb14066cebb9eda6196b545334283e865943
Yurii Kolesnikov <yk@vcv.ru>
2020-10-04 17:53:07 +0700

    Added get data request in vuex actions

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <div id="app">
     <navbar/>
-    <div class="main"></div>
+    <div class="main">
+      Count: {{ count }}<br>
+      Next: {{ next }}<br>
+      Prev: {{ previous }}
+      Data: {{ data }}
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
 import Navbar from '@/components/Navbar'
 export default {
   components: {
     Navbar
+  },
+  methods: {
+    ...mapActions(['GET_DATA'])
+  },
+  computed: {
+    ...mapState(['count', 'next', 'previous', 'data'])
+  },
+  mounted() {
+    this.GET_DATA({
+      type: 'regular'
+    })
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { api } from '@/services/api'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: null,
+    next: null,
+    previous: null,
+    data: null
   },
   mutations: {
+    SAVE_DATA: (state, { count, next, previous, results }) => {
+      state.count     = count
+      state.next      = next
+      state.previous  = previous
+      state.data      = results
+    }
   },
   actions: {
+    GET_DATA: async ({ state, commit }, { type = 'regular', page = 1 }) => {
+      const requestTypes = {
+        'next': state.next,
+        'previous': state.previous,
+        'page': `https://swapi.dev/api/vehicles/?page=${page}`,
+        'regular': 'https://swapi.dev/api/vehicles'
+      }
+      
+      const response = await api.get(requestTypes[type])
+      commit('SAVE_DATA', response.data)
+    },
   },
-  modules: {
+  getters: {
+
   }
 })
