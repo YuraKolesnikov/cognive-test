1dbc82add12285229682029f847cc181ec73e401
Yurii Kolesnikov <yk@vcv.ru>
2020-10-05 17:54:10 +0700

    Added qs lib and query stringing logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import qs from 'qs'
 
 import { api } from '@/services/api'
 
@@ -36,19 +37,34 @@ export default new Vuex.Store({
       state.sort.ascending = !state.sort.ascending
       state.sort.id = id
     },
+    SET_PAGE: (state, page) => state.currentPage = page,
+    PREV_PAGE: state => state.currentPage -= 1,
+    NEXT_PAGE: state => state.currentPage += 1,
     UPDATE_FILTER_KEY: (state, key) => state.filter.key = key,
     UPDATE_FILTER_VAL: (state, val) => state.filter.value = val
   },
   actions: {
     GET_DATA: async ({ state, commit }, { type = 'regular', page = 1 }) => {
-      const requestTypes = {
-        'next': state.next,
-        'previous': state.previous,
-        'page': `https://swapi.dev/api/vehicles/?page=${page}`,
-        'regular': 'https://swapi.dev/api/vehicles'
+      switch (type) {
+        case 'next':
+          commit('NEXT_PAGE')
+          break;
+        case 'previous':
+          commit('PREV_PAGE')
+          break
+        case 'page':
+          commit('SET_PAGE', page)
+        default:
+          break;
       }
 
-      const response = await api.get(requestTypes[type])
+      const payload = {
+        page: state.currentPage,
+        /* Do not create 'search' key-value pair if state.filter.value is empty (to avoid this kind of string - /?page=1&search=) */
+        ...(!state.filter.value ? {} : { search: state.filter.value })
+      }
+      
+      const response = await api.get(`?${qs.stringify(payload)}`)
       response.data.results.forEach(item => normalize(item))
 
       commit('SAVE_DATA', response.data)
@@ -57,7 +73,8 @@ export default new Vuex.Store({
   getters: {
     localData: state => {
       let data
-      if (!!state.filter.value) {
+      /* Check if there is a filter value and prevent filtering by name */
+      if (!!state.filter.value && state.filter.key !== 'name') {
         data = state.data.filter(item => item[state.filter.key].match(state.filter.value))
       } else {
         data = state.data
